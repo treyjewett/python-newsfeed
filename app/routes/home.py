dc85f19ac151a69f56723eff44ab7064174a3113
Russell Jewett <rfjewett3@gmail.com>
2021-06-11 14:51:12 -0400

    adds homepage and single post routes and incorporates db into the render.

--- app/routes/home.py
+++ app/routes/home.py
@@ -17,5 +17,10 @@ def login():
   return render_template('login.html')
 
 @bp.route('/post/<id>')
-def sing(id):
-  return render_template('single-post.html')
+def single(id):
+  #get single post by id
+  db = get_db()
+  post = db.query(Post).filter(Post.id == id).one()
+
+  #render single post template
+  return render_template('single-post.html', post=post)
