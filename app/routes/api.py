2c0df4dee76a10d7e16d50326bd2e8237202b898
Russell Jewett <rfjewett3@gmail.com>
2021-06-12 21:54:27 -0400

    sign up, login and log-out routes are all functional. Sessions are now attributed when a user logs in.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify #type:ignore
+from flask import Blueprint, request, jsonify, session #type:ignore
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -21,7 +22,41 @@ def signup():
     db.add(newUser)
     db.commit()
   except:
+    #print the error to the console to help identify the issue
+    print(sys.exc_info()[0])
     #insert failed, so send error to front end
+    db.rollback()
     return jsonify(message = 'Signup Failed'), 500
 
+  session.clear()
+  session['user_id'] = newUser.id
+  session['loggedIn'] = True
+
   return jsonify(id = newUser.id)
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+  #login an existing user if they exist in the db
+  data = request.get_json()
+  db = get_db()
+  #check to see if the user exists in the db
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+  except:
+    print(sys.exc_info()[0])
+    return jsonify(message = 'Incorrect Credentials'), 400
+
+  if user.verify_password(data['password']) == False:
+    return jsonify(message = 'Incorrect credentials'), 400
+
+  session.clear()
+  session['user.id'] = user.id
+  session['loggedIn'] = True
+
+  return jsonify(id = user.id)
+
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+  #remove session variables
+  session.clear()
+  return '', 204
