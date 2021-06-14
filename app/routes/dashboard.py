681c12b20ed48998c9aa20f9d26d1bc93677448f
Russell Jewett <rfjewett3@gmail.com>
2021-06-14 16:54:36 -0400

    updates dashboard to reflect posts that the user has created.

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template #type:ignore
+from flask import Blueprint, render_template, session #type:ignore
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
-  return render_template('dashboard.html')
+  db = get_db()
+  posts = (
+    db.query(Post)
+    .filter(Post.user_id == session.get('user_id'))
+    .order_by(Post.created_at.desc())
+    .all()
+  )
+  return render_template(
+    'dashboard.html',
+    posts=posts,
+    loggedIn=session.get('loggedIn')
+  )
 
 @bp.route('/edit/<id>')
 def edit(id):
